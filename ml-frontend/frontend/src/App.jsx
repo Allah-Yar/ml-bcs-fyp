9cf2cd48dc6e68dcd0b53fb3c9fdbf71da9f67a6
Allah-Yar <atebarwaxir4455@gmail.com>
2025-03-10 16:19:35 +0500

    add vercel.json

--- ml-frontend/frontend/src/App.jsx
+++ ml-frontend/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 
 
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Navbar from "./components/Navbar";
 import GrokBackground from "./GrokBackground";
